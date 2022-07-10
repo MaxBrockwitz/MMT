c45361a4a7de68c50d7f146adccd367b80aa5082
Razewing27 <maxbrockwitz@yahoo.de>
2022-07-10 14:39:47 +0200

    Initial Commit

---
+++ MMT/Controllers/FinanceController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MMT.Controllers
+{
+    public class FinanceController : Controller
+    {
+        [HttpGet]
+        [Route("Finance/GetMonthly")]
+        public IActionResult GetMonthyOverview(int month, int UserID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
